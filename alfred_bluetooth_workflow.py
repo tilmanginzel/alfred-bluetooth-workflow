afba424ea731eaae6e1aad00396f38ebff11b1dd
Robin Trietsch <rtrietsch@bol.com>
2019-01-24 22:40:49 +0100

    Added auto update functionality

--- alfred_bluetooth_workflow.py
+++ alfred_bluetooth_workflow.py
@@ -3,10 +3,15 @@ import subprocess
 import sys
 
 from lxml import etree
+from workflow import Workflow
+
+log = None
+GITHUB_SLUG = 'trietsch/temp-alfred-bluetooth-workflow'
 
 
 def _find_element_by_key(device, key):
-    elements = device.xpath(".//*[text()='{}']/following-sibling::string[1]".format(key))
+    elements = device.xpath(
+        ".//*[text()='{}']/following-sibling::string[1]".format(key))
     return elements[0] if len(elements) else None
 
 
@@ -21,34 +26,57 @@ def _read_boolean_value(device, key):
 
 
 def _read_devices():
-    proc = subprocess.Popen(['system_profiler', 'SPBluetoothDataType', '-xml'], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(
+        ['system_profiler', 'SPBluetoothDataType', '-xml'], stdout=subprocess.PIPE)
     bluetooth_devices_xml_raw = proc.stdout.read()
-    bluetooth_devices_xml = etree.fromstring(bluetooth_devices_xml_raw).xpath("//*[text()='device_title']/following-sibling::array[1]/dict")
+    bluetooth_devices_xml = etree.fromstring(bluetooth_devices_xml_raw).xpath(
+        "//*[text()='device_title']/following-sibling::array[1]/dict")
 
     bluetooth_devices = []
 
     for device in bluetooth_devices_xml:
         is_connected = _read_boolean_value(device, 'device_isconnected')
         battery = _read_string_value(device, 'device_batteryPercent')
-        subtitle = ('Connected' if is_connected else 'Disconnected') + (', ' + battery if battery else '')
+        subtitle = ('Connected' if is_connected else 'Disconnected') + \
+            (', ' + battery if battery else '')
 
         bluetooth_devices.append({
-            'type': 'file',
+            'type': 'file:skipcheck',
             'arg': _read_string_value(device, 'device_addr'),
             'subtitle': subtitle,
             'connected': is_connected,
             'title': device.xpath('.//key[1]')[0].text,
-            'icon': {
-                'path': './icons/bluetooth-' + ('connected' if is_connected else 'disconnected') + '.png'
-            }
+            'icon': './icons/bluetooth-' + ('connected' if is_connected else 'disconnected') + '.png'
         })
 
     return bluetooth_devices
 
 
-if __name__ == '__main__':
-    query = sys.argv[1] if len(sys.argv) > 0 else ''
+def main(wf):
+    if wf.update_available:
+        wf.add_item('Update available for Bluetooth Connector!',
+                    autocomplete='workflow:update',
+                    valid=False)
 
+    query = wf.args[0] if len(wf.args) else None
     devices = _read_devices()
-    filtered_devices = filter(lambda x: query.strip().lower() in x['title'].lower(), devices)
-    print(json.dumps({"items": sorted(filtered_devices, key=lambda d: (not d['connected'], d['title']))}))
+
+    filtered_devices = wf.filter(query, devices, key=lambda k: k['title'])
+
+    for device in filtered_devices:
+        wf.add_item(
+            type=device['type'],
+            title=device['title'],
+            subtitle=device['subtitle'],
+            arg=device['arg'],
+            icon=device['icon'],
+            valid=True
+        )
+
+    wf.send_feedback()
+
+
+if __name__ == '__main__':
+    wf = Workflow(update_settings={'github_slug': GITHUB_SLUG})
+    log = wf.logger
+    sys.exit(wf.run(main))
